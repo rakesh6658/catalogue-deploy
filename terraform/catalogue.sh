0daacf2f33530808114dacdb1041abdbb9e3982d
rakesh <rakeshreddy@gmail.com>
2025-08-04 17:53:18 +0200

    added all

--- terraform/catalogue.sh
+++ terraform/catalogue.sh
@@ -2,7 +2,14 @@
 APP_VERSION=$1
 ENVIRONMENT=$2
 echo "app version: $APP_VERSION"
-yum install python3.11-devel python3.11-pip -y
-pip3.11 install ansible botocore boto3
+
+# Use system package manager instead of pip
+dnf install ansible -y
+
+# Run ansible-pull
 cd /tmp
-ansible-pull -U https://github.com/rakesh6658/ansible-roboshop-roles-2025-tf.git -e component=catalogue -e app_version=$APP_VERSION -e env=$ENVIRONMENT main.yaml
+ansible-pull -U https://github.com/rakesh6658/ansible-roboshop-roles-2025-tf.git \
+  -e component=catalogue \
+  -e app_version=$APP_VERSION \
+  -e env=$ENVIRONMENT \
+  main.yaml
